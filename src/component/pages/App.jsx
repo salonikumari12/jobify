1e46a88259eb5e2ddd2b92d75683ccdde4274af7
salonikumari12 <162488452+salonikumari12@users.noreply.github.com>
2025-06-20 13:25:00 +0530

    Add files via upload

---
+++ src/component/pages/App.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Signup from './pages/Signup';
+import Dashboard from './pages/Dashboard';
+import Jobs from './pages/Jobs';
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/jobs" element={<Jobs />} />
+      </Routes>
+    </>
+  );
+}
+
+export default App;
